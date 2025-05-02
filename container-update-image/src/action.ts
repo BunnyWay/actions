ca923a564c0ba3ebd1f4d43612bc56657952a8cd
Gordy French <gordyf@gordyf.com>
2025-05-02 15:58:30 -0700

    Undo reformatting

--- container-update-image/src/action.ts
+++ container-update-image/src/action.ts
@@ -28,129 +28,113 @@ export async function run() {
 
     await saveAppConfiguration(token, appId, appConfig);
   } catch (e) {
-    if (typeof e === "string" || e instanceof Error) {
+    if (typeof e === 'string' || e instanceof Error) {
       core.setFailed(e);
     } else {
-      core.setFailed("Unexpected error");
+      core.setFailed('Unexpected error');
     }
   }
 }
 
 async function exchangeApiKeyForToken(apiKey: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    fetch("https://api.bunny.net/apikey/exchange", {
-      method: "POST",
+    fetch('https://api.bunny.net/apikey/exchange', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        AccessKey: apiKey,
+        'Content-Type': 'application/json',
+        'AccessKey': apiKey,
       },
       body: JSON.stringify({ AccessKey: apiKey }),
     })
-      .then((response) => {
+      .then(response => {
         if (response.status === 401) {
-          reject("Invalid api_key.");
+          reject('Invalid api_key.');
           return;
         }
 
         if (response.status !== 200) {
-          reject(
-            `Could not obtain access token: HTTP status ${response.status}.`
-          );
+          reject(`Could not obtain access token: HTTP status ${response.status}.`);
           return;
         }
 
-        response
-          .json()
-          .then((obj) => {
+        response.json()
+          .then(obj => {
             resolve(obj.Token);
           })
-          .catch((e) => {
+          .catch(e => {
             console.log(e);
-            reject("Could not parse JSON response.");
-          });
+            reject('Could not parse JSON response.');
+          })
+        ;
       })
-      .catch((e) => {
+      .catch(e => {
         console.log(e);
-        reject("Could not obtain access token.");
+        reject('Could not obtain access token.');
       });
   });
 }
 
-async function getAppConfiguration(
-  token: string,
-  appId: string
-): Promise<AppConfiguration> {
+async function getAppConfiguration(token: string, appId: string): Promise<AppConfiguration> {
   return new Promise((resolve, reject) => {
-    fetch(
-      `https://api-mc.opsbunny.net/v1/namespaces/default/applications/${appId}/configuration`,
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          Authorization: token,
-        },
-      }
-    )
-      .then((response) => {
+    fetch(`https://api-mc.opsbunny.net/v1/namespaces/default/applications/${appId}/configuration`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': token
+      },
+    })
+      .then(response => {
         if (response.status === 400) {
-          reject(
-            `Could not obtain app configuration: Double-check your app_id.`
-          );
+          reject(`Could not obtain app configuration: Double-check your app_id.`);
           return;
         }
 
         if (response.status !== 200) {
-          reject(
-            `Could not obtain app configuration: HTTP status ${response.status}.`
-          );
+          reject(`Could not obtain app configuration: HTTP status ${response.status}.`);
           return;
         }
 
-        response
-          .json()
-          .then((obj) => {
+        response.json()
+          .then(obj => {
             resolve(obj);
           })
-          .catch((e) => {
+          .catch(e => {
             console.log(e);
-            reject("Could not parse JSON response.");
-          });
+            reject('Could not parse JSON response.');
+          })
+        ;
       })
-      .catch((e) => {
+      .catch(e => {
         console.log(e);
-        reject("Could not obtain app configuration.");
-      });
+        reject('Could not obtain app configuration.');
+      })
+    ;
   });
 }
 
-async function saveAppConfiguration(
-  token: string,
-  appId: string,
-  appConfig: AppConfiguration
-): Promise<void> {
+async function saveAppConfiguration(token: string, appId: string, appConfig: AppConfiguration): Promise<void> {
   return new Promise((resolve, reject) => {
-    fetch("https://api-mc.opsbunny.net/v1/namespaces/default/applications", {
-      method: "PUT",
+    fetch('https://api-mc.opsbunny.net/v1/namespaces/default/applications', {
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: token,
+        'Content-Type': 'application/json',
+        'Authorization': token
       },
       body: JSON.stringify(appConfig),
     })
-      .then((response) => {
+      .then(response => {
         if (response.status !== 200) {
-          reject(
-            `Could not save app configuration: HTTP status ${response.status}.`
-          );
+          reject(`Could not save app configuration: HTTP status ${response.status}.`);
           return;
         }
 
         resolve();
       })
-      .catch((e) => {
+      .catch(e => {
         console.log(e);
-        reject("Could not save app configuration.");
-      });
+        reject('Could not save app configuration.');
+      })
+    ;
   });
 }
 
@@ -161,4 +145,4 @@ type AppConfiguration = {
     imageTag: string;
     imageDigest?: string;
   }>;
-};
+}
